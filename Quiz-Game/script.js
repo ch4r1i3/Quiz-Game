2f72b602f1f2cf7479b920b4909973fc1f1ad8a1
ch4r1i3 <111701536+ch4r1i3@users.noreply.github.com>
2022-10-29 17:43:06 -0400

    Corrections

--- Quiz-Game/script.js
+++ Quiz-Game/script.js
@@ -1,4 +1,4 @@
-var strCard = document.querySelector("intro");
+var strCard = document.querySelector(".intro");
 var strBtn = document.getElementById("strBtn");
 var timer = document.getElementById("timer");
 var quiz = document.getElementById("quiz");
@@ -7,13 +7,37 @@ var endpart = document.getElementById("endpart");
 var finalScore = document.getElementById("score");
 var timeLeft = 90;
 var newScore = document.getElementById("olhs");
+var questions =  [
+	{
+		question: "Inside which HTML element do we put the JavaScript?",
+		answers: ['<script>','<scripting>','<js>','<javascript>'],
+		correctAnswer: '<script>'
+	},
+	{
+		question: "Where is the correct place to insert a JavaScript?",
+		answers: [ 'The <body> section.', 'The <head> section.', 'Both the <head> and <body> section are correct.', 'None of those two'],
+		correctAnswer: 'Both the <head> and <body> section are correct.'
+	},
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answers: ['<srcript name = xxx.js>', '<script href = xxx.js>', '<script src = xxx.js.>','None of the above'],
+        correctAnswer:'<script src = xxx.js>'
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: ['alert("HelloWorld)', 'msg("HelloWorld)','alertBox("HelloWorld)','msgBox("HelloWorld)'],
+        correctAnswer:'alert("HelloWorld)'
+    },
+];
+
 
 //  Here is the start button function//
 strBtn.addEventListener("click", countDown);
 
 strBtn.addEventListener("click", function() {
     strCard.style.display = "none";
-    quiz.style.display = "block";
+    question.style.display = "flex";
+    optionsBtn.style.display = "flex";
 })
 
 
@@ -35,7 +59,7 @@ function countDown() {
     if (timeLeft < 1 || indexQuestion >= questions.length) {
     timer.textContent = "0";
     clearInterval(timeInterval);
-    container.style.display = "none";
+    quiz.style.display = "none";
     endpart.style.display = "block";
     }
     }, 1000);
@@ -46,13 +70,12 @@ let indexQuestion = 0
 
 loadQuestion(indexQuestion)
 
-
 function loadQuestion(index) {
     objectQuestion = questions[index];
-    options = [...objectQuestion.distractions];
-    options.push(objectQuestion.answer);;
-    options.sort(() => Math.random() - 0.5);
-    document.getElementById("question1").innerHTML = objectQuestion.question
+    console.log(objectQuestion) 
+    let options = objectQuestion.answers
+    console.log(options)
+    document.getElementById("question").innerHTML = objectQuestion.question
     document.getElementById("response1").innerHTML = options[0];
     document.getElementById("response2").innerHTML = options[1];
     document.getElementById("response3").innerHTML = options[2];
@@ -101,7 +124,7 @@ submitbtn.addEventListener("click", saveToLocalStorage)
 
 // This functions adds info to the local storage
 function addScores() {
-        newScore.innerHTML = ""
+        // newScore.innerHTML = ""
     var highScore = JSON.parse(localStorage.getItem("highScore"))
     for (i = 0; i < highScore.length; i++) {
     var newLi = document.createElement("li");
@@ -118,24 +141,23 @@ function addScores() {
     })
     }
 }
-addScores()
+// addScores()
 
 //  This section sets the functionality of the finish button
 var goBackBtn = document.getElementById("goback");
 var hScore = document.getElementById("highScores")
 var clearBtn = document.getElementById("clear");
 
-goBackBtn.addEventListener("click", function () {
-    highScore.style.display = "none";
-    strCard.style.display = "block";
+// goBackBtn.addEventListener("click", function () {
+//     highScore.style.display = "none";
+//     strCard.style.display = "block";
 
-})
+// })
 
 // Highscore to record users performance
 var viewHSbtn = document.getElementById("highScores");
 
-viewHSbtn.addEventListener("click", function () {
-    strCard.style.display = "none";
-    endpart.style.display = "none";
-    highScore.style.display = "block";
-});
+// viewHSbtn.addEventListener("click", function () {
+//     strCard.style.display = "none";
+//     endpart.style.display = "none";
+//     highScore.style.display = "block";
